130a96525fba7a7fddcb4a7d924f8cd1bc2aef6e
Roopak Venkatakrishnan <roopak.v@gmail.com>
2018-05-19 23:23:45 -0700

    first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,24 @@
 module.exports = {
   'extends': ['airbnb-base', 'plugin:prettier/recommended'],
-  'plugins': ['prettier'],
+  'plugins': [
+    'prettier', 
+    'mocha'
+  ],
+  env: {
+    es6: true,
+    mocha: true,
+    node: true
+  },
   'rules': {
     'prettier/prettier': 'error',
-    'strict': 0
+    strict: 0,
+    'mocha/no-exclusive-tests': 'error',
+    'mocha/no-mocha-arrows': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-return-and-callback': 'error',
+    'mocha/no-skipped-tests': 'error',
+    'prefer-arrow-callback': 0,
+    'import/no-extraneous-dependencies': 0, 
+    'func-names': 0
   }
 };
