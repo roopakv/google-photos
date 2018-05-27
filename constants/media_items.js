285f08ce79943dead64dbaafd6317b8a79d1cd02
Roopak Venkatakrishnan <roopak.v@gmail.com>
2018-05-26 17:48:05 -0700

    start adding media items

---
+++ constants/media_items.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const MEDIA_ITEMS = {
+  BASE_PATH: '/v1/mediaItems',
+  VIDEO_PROCESSING_STATUS: {
+    UNSPECIFIED: 'UNSPECIFIED',
+    PROCESSING: 'PROCESSING',
+    READY: 'READY',
+    FAILED: 'FAILED'
+  }
+};
+
+module.exports = MEDIA_ITEMS;
