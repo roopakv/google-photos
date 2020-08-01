d45b7ec7c538e06b3c2993216d159d74e45eb486
Roopak Venkatakrishnan <roopak.v@gmail.com>
2020-08-01 11:10:03 -0700

    Fix endpoints

--- constants/album.js
+++ constants/album.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const ALBUM = {
-  BASE_PATH: '/v1/albums',
+  BASE_PATH: 'v1/albums',
   ENRICHMENTS: {
     TEXT: 'text',
     LOCATION: 'location',
--- constants/media_items.js
+++ constants/media_items.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const MEDIA_ITEMS = {
-  BASE_PATH: '/v1/mediaItems',
+  BASE_PATH: 'v1/mediaItems',
   VIDEO_PROCESSING_STATUS: {
     UNSPECIFIED: 'UNSPECIFIED',
     PROCESSING: 'PROCESSING',
